c249a01879375ee75277cb9a9864d71100c53559
michalAndrzejewski <pl.michal.and@gmail.com>
2022-12-13 16:11:28 +0100

    More updates

--- Warehouse/app/models.py
+++ Warehouse/app/models.py
@@ -11,11 +11,18 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """
+    SENIORITY = [
+        ('JUNIOR', 'Junior'),
+        ('MID', 'Middle'),
+        ('SENIOR', 'Senior'),
+    ]"""
     """Model for a products table in Northwind database"""
     product_name = models.CharField(max_length=25)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     unit_price = models.FloatField(default=0)
     units_in_stock = models.IntegerField(default=0)
+    """choices = models.CharField(max_length=20,choices=SENIORITY, default='Junior')"""
 
     def __str__(self):
         return self.product_name
